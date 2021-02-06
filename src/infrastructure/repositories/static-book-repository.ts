ad2e5056485c635adee2a03423ab77d4271525a3
abekoh <abekoh.bcky@gmail.com>
2021-02-06 23:51:18 +0900

    update

---
+++ src/infrastructure/repositories/static-book-repository.ts
@@ -0,0 +1,34 @@
+import IBookRepository from '../../domain/repositories/book-repository';
+import { Book } from '../../domain/models/book';
+
+const staticBooks: Book[] = [
+  {
+    title: 'ドメイン駆動設計入門',
+    subtitle: 'ボトムアップでわかる！ドメイン駆動設計の基本',
+    price: 3520,
+    url: 'https://www.shoeisha.co.jp/book/detail/9784798150727',
+    tags: [{ name: 'DDD' }],
+    isCompleted: true,
+  },
+  {
+    title: 'リファクタリング',
+    subtitle: '既存のコードを安全に改善する',
+    price: 4840,
+    url: 'https://www.ohmsha.co.jp/book/9784274224546/',
+    tags: [],
+    isCompleted: false,
+  },
+  {
+    title: 'マイクロサービスアーキテクチャ',
+    price: 3740,
+    url: 'https://www.oreilly.co.jp/books/9784873117607/',
+    tags: [],
+    isCompleted: false,
+  },
+];
+
+class StaticBookRepository implements IBookRepository {
+  getBooks: () => Book[] = () => staticBooks;
+}
+
+export default StaticBookRepository;
