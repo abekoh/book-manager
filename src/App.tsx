c742d09d40e861131d981fcb010561630bdb551d
Kotaro Abe <abekoh.bcky@gmail.com>
2021-02-11 15:12:07 +0900

    update route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React, { FC } from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import BookList from './containers/organisms/BookList';
 import BookDetail from './containers/molecules/BookDetail';
 
 const App: FC = () => (
   <>
     <Routes>
-      <Route path="books">
-        <Route path="/" element={<BookList />} />
-        <Route path=":bookId" element={<BookDetail />} />
+      <Route path='books'>
+        <Route path='/' element={<BookList />} />
+        <Route path=':bookId' element={<BookDetail />} />
       </Route>
-      {/* <Route path="*" element={<Navigate to="/books" />} /> */}
+      <Route path="*" element={<Navigate to="/books" />} />
     </Routes>
   </>
 );
