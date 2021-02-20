d066ff8f7a7f86fd113dc615e27fb16560307a1a
abekoh <abekoh.bcky@gmail.com>
2021-02-20 20:36:31 +0900

    update

--- src/infrastructure/gateways/firestore-book-repository.ts
+++ src/infrastructure/gateways/firestore-book-repository.ts
@@ -15,23 +15,32 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
+type BookDoc = {
+  title: string;
+  subtitle?: string;
+  price: number;
+  url?: string;
+  tags: string[];
+  isCompleted: boolean;
+};
+
 const FirestoreBookRepository: IBookRepository = {
   getBooks: async () => {
     const snapShot = await db.collection('books').get();
     const result: Book[] = snapShot.docs.map((doc: QueryDocumentSnapshot) => {
-      const data = doc.data();
+      const data = doc.data() as BookDoc;
 
-      /* eslint-disable @typescript-eslint/no-unsafe-assignment */
       return {
-        id: 'a',
+        id: doc.id,
         title: data.title,
         subtitle: data.subtitle,
         price: data.price,
         url: data.url,
-        tags: [],
+        tags: data.tags.map((str) => ({
+          name: str,
+        })),
         isCompleted: data.isCompleted,
       };
-      /* eslint-enable */
     });
 
     return Promise.resolve(result);
