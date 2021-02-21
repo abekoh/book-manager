296f762f01658742c9cd12cfe2e862f1ca68764d
abekoh <abekoh.bcky@gmail.com>
2021-02-21 16:30:33 +0900

    update

--- src/components/molecules/BookNew.tsx
+++ src/components/molecules/BookNew.tsx
@@ -1,12 +1,21 @@
-import { FC } from 'react';
+import { ChangeEvent, FC, useState } from 'react';
 import {
   Card,
   CardContent,
+  Chip,
   createStyles,
+  FormControl,
+  Grid,
+  Input,
+  InputAdornment,
+  InputLabel,
   makeStyles,
+  MenuItem,
+  Select,
   TextField,
   Theme,
 } from '@material-ui/core';
+import { TAG_PRESETS } from '../../domain/models/book';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,12 +29,63 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const BookNew: FC = () => {
   const classes = useStyles();
+  const [tags, setTags] = useState<string[]>([]);
+
+  const handleChange = (event: ChangeEvent<{ value: unknown }>) => {
+    setTags(event.target.value as string[]);
+  };
 
   return (
     <Card>
       <CardContent>
         <form className={classes.root}>
-          <TextField id="title" label="タイトル" />
+          <Grid container direction="column" spacing={1}>
+            <Grid item>
+              <TextField id="title" label="タイトル" />
+            </Grid>
+            <Grid item>
+              <TextField id="subtitle" label="サブタイトル" />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="price"
+                label="値段"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">￥</InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <TextField id="url" label="URL" />
+            </Grid>
+            <Grid item>
+              <FormControl>
+                <InputLabel>タグ</InputLabel>
+                <Select
+                  id="tags"
+                  multiple
+                  input={<Input id="tags" />}
+                  value={tags}
+                  onChange={handleChange}
+                  renderValue={(selected) => (
+                    <div>
+                      {selected.map((value) => (
+                        <Chip key={value} label={value} />
+                      ))}
+                    </div>
+                  )}
+                >
+                  {TAG_PRESETS.map((name) => (
+                    <MenuItem key={name} value={name}>
+                      {name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
         </form>
       </CardContent>
     </Card>
--- src/domain/models/book.ts
+++ src/domain/models/book.ts
@@ -13,3 +13,6 @@ export type BookWithoutId = Omit<Book, 'id'>;
 export type Tag = {
   name: string;
 };
+
+export const TAG_PRESETS = ['設計', 'DDD'] as const;
+export type TagPresets = typeof TAG_PRESETS;
