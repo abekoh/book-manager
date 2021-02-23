3e41a6b17ac827b8b826e956f757759f9d83c29a
abekoh <abekoh.bcky@gmail.com>
2021-02-23 19:22:39 +0900

    update

--- src/infrastructure/gateways/firestore-book-repository.test.ts
+++ src/infrastructure/gateways/firestore-book-repository.test.ts
@@ -1,22 +1,22 @@
 import FirestoreBookRepository from './firestore-book-repository';
 
+const sampleDoc = {
+  id: '550e8400-e29b-41d4-a716-446655440000',
+  data: () => ({
+    title: 'ドメイン駆動設計入門',
+    subtitle: 'ボトムアップでわかる！ドメイン駆動設計の基本',
+    price: 3520,
+    url: 'https://www.shoeisha.co.jp/book/detail/9784798150727',
+    tags: ['DDD', '設計'],
+    isCompleted: true,
+  }),
+};
+
 jest.mock('./firestore-db', () => ({
   collection: () => ({
     get: jest.fn(() =>
       Promise.resolve({
-        docs: [
-          {
-            id: '550e8400-e29b-41d4-a716-446655440000',
-            data: jest.fn(() => ({
-              title: 'ドメイン駆動設計入門',
-              subtitle: 'ボトムアップでわかる！ドメイン駆動設計の基本',
-              price: 3520,
-              url: 'https://www.shoeisha.co.jp/book/detail/9784798150727',
-              tags: ['DDD', '設計'],
-              isCompleted: true,
-            })),
-          },
-        ],
+        docs: [sampleDoc],
       }),
     ),
   }),
