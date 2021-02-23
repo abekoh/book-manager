060e7da64c84b9ba9609ac168804f81603decac4
abekoh <abekoh.bcky@gmail.com>
2021-02-23 19:29:01 +0900

    update

--- src/infrastructure/gateways/firestore-book-repository.test.ts
+++ src/infrastructure/gateways/firestore-book-repository.test.ts
@@ -1,24 +1,33 @@
 import FirestoreBookRepository from './firestore-book-repository';
 
-const sampleDoc = {
-  id: '550e8400-e29b-41d4-a716-446655440000',
-  data: () => ({
-    title: 'ドメイン駆動設計入門',
-    subtitle: 'ボトムアップでわかる！ドメイン駆動設計の基本',
-    price: 3520,
-    url: 'https://www.shoeisha.co.jp/book/detail/9784798150727',
-    tags: ['DDD', '設計'],
-    isCompleted: true,
-  }),
-};
+const sampleDataFunc = () => ({
+  title: 'ドメイン駆動設計入門',
+  subtitle: 'ボトムアップでわかる！ドメイン駆動設計の基本',
+  price: 3520,
+  url: 'https://www.shoeisha.co.jp/book/detail/9784798150727',
+  tags: ['DDD', '設計'],
+  isCompleted: true,
+});
 
 jest.mock('./firestore-db', () => ({
   collection: () => ({
     get: jest.fn(() =>
       Promise.resolve({
-        docs: [sampleDoc],
+        docs: [
+          {
+            id: '550e8400-e29b-41d4-a716-446655440000',
+            data: sampleDataFunc,
+          },
+        ],
       }),
     ),
+    doc: jest.fn(() => ({
+      get: jest.fn(() =>
+        Promise.resolve({
+          data: sampleDataFunc,
+        }),
+      ),
+    })),
   }),
 }));
 
@@ -27,6 +36,23 @@ describe('getBooks すべての本の情報を取得する', () => {
     const actual = await FirestoreBookRepository.getBooks();
     expect(actual.length).toBe(1);
     expect(actual[0]).toEqual({
+      id: '550e8400-e29b-41d4-a716-446655440000',
+      title: 'ドメイン駆動設計入門',
+      subtitle: 'ボトムアップでわかる！ドメイン駆動設計の基本',
+      price: 3520,
+      url: 'https://www.shoeisha.co.jp/book/detail/9784798150727',
+      tags: [{ name: 'DDD' }, { name: '設計' }],
+      isCompleted: true,
+    });
+  });
+});
+
+describe('getOneBook', () => {
+  test('期待した本の情報が返され、正しくマッピングされる。', async () => {
+    const actual = await FirestoreBookRepository.getOneBook(
+      '550e8400-e29b-41d4-a716-446655440000',
+    );
+    expect(actual).toEqual({
       id: '550e8400-e29b-41d4-a716-446655440000',
       title: 'ドメイン駆動設計入門',
       subtitle: 'ボトムアップでわかる！ドメイン駆動設計の基本',
